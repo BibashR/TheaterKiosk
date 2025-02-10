c933dd1ae1f76535f08ddf6cd60d0e58327bd145
brwak <wakchhalibr@gmail.com>
2025-02-09 22:09:45 -0500

    Completed Task 4

--- src/TheaterKiosk.java
+++ src/TheaterKiosk.java
@@ -1,5 +1,16 @@
-public class Main {
+import java.util.Scanner;
+
+public class TheaterKiosk {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your age: ");
+        if (scanner.hasNextInt()) {
+            int age = scanner.nextInt();
+            if (age >= 21) {
+                System.out.println("You get a wristband."); }
+        }
+
+        scanner.close();
     }
 }
